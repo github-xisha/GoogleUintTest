b68e76dd977a4640072c257cbdcabea8d65d77b7
github-xisha <reader_xisha@163.com>
2018-09-02 22:08:52 +0800

    delete googletest

--- googletest/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xisha/MyGitHub/GoogleUnitTest/googletest
-# Build directory: /home/xisha/MyGitHub/GoogleUnitTest/googletest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
